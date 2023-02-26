c4849760347ce9e36f01511f38822d3cca420849
Glazov Nikita <niglazov@gmail.com>
2023-02-26 13:51:25 +0300

    fixed variable names

--- src/CreditService.java
+++ src/CreditService.java
@@ -1,9 +1,9 @@
 public class CreditService {
-    public double calculate(int CreditSumma, double NumberOfMonths) {
+    public double calculate(int creditSumma, double numberOfMonths) {
         double percent = 9.99;
-        double MonthlyInterestRate = percent / (100 * 12);
-        double Payment = CreditSumma * (MonthlyInterestRate / (1 - Math.pow((1 + MonthlyInterestRate), -NumberOfMonths)));
-        return Payment;
+        double monthlyInterestRate = percent / (100 * 12);
+        double payment = creditSumma * (monthlyInterestRate / (1 - Math.pow((1 + monthlyInterestRate), -numberOfMonths)));
+        return payment;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         CreditService service = new CreditService();
-        int CreditSumma = 1_000_000;
-        int NumberOfMonths = 12;
-        double Payment = service.calculate(CreditSumma, NumberOfMonths);
-        System.out.println("Платёж в месяц составляет " + Payment + " Число месяцев равно " + NumberOfMonths);
-        NumberOfMonths = 24;
-        Payment = service.calculate(CreditSumma, NumberOfMonths);
-        System.out.println("Платёж в месяц составляет " + Payment + " Число месяцев равно " + NumberOfMonths);
-        NumberOfMonths = 36;
-        Payment = service.calculate(CreditSumma, NumberOfMonths);
-        System.out.println("Платёж в месяц составляет " + Payment + " Число месяцев равно " + NumberOfMonths);
+        int creditSumma = 1_000_000;
+        int numberOfMonths = 12;
+        double payment = service.calculate(creditSumma, numberOfMonths);
+        System.out.println("Платёж в месяц составляет " + payment + " Число месяцев равно " + numberOfMonths);
+        numberOfMonths = 24;
+        payment = service.calculate(creditSumma, numberOfMonths);
+        System.out.println("Платёж в месяц составляет " + payment + " Число месяцев равно " + numberOfMonths);
+        numberOfMonths = 36;
+        payment = service.calculate(creditSumma, numberOfMonths);
+        System.out.println("Платёж в месяц составляет " + payment + " Число месяцев равно " + numberOfMonths);
 
     }
 }
